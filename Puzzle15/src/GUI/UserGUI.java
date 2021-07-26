225c57fe6c6d11aa21ca4e50cd7740ab74883fef
presi911 <dfclavijo@gmail.com>
2021-07-26 13:50:12 -0500

    UserGUI v1.0

--- Puzzle15/src/GUI/UserGUI.java
+++ Puzzle15/src/GUI/UserGUI.java
@@ -16,6 +16,7 @@ public class UserGUI extends java.awt.Frame {
      */
     public UserGUI() {
         initComponents();
+        this.setLocationRelativeTo(null);
     }
 
     /**
@@ -26,11 +27,233 @@ public class UserGUI extends java.awt.Frame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+        jButton6 = new javax.swing.JButton();
+        jButton7 = new javax.swing.JButton();
+        jButton8 = new javax.swing.JButton();
+        jButton9 = new javax.swing.JButton();
+        jButton10 = new javax.swing.JButton();
+        jButton12 = new javax.swing.JButton();
+        jButton11 = new javax.swing.JButton();
+        jButton13 = new javax.swing.JButton();
+        jButton14 = new javax.swing.JButton();
+        jButton16 = new javax.swing.JButton();
+        btnAlter = new javax.swing.JButton();
+        jButton17 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        btnSolve = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+
+        setUndecorated(true);
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowClosing(java.awt.event.WindowEvent evt) {
                 exitForm(evt);
             }
         });
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/Img/1.PNG"))); // NOI18N
+        jButton1.setText("1");
+        jButton1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 200, 70, 60));
+
+        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/Img/2.PNG"))); // NOI18N
+        jButton2.setText("jButton1");
+        jButton2.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 200, 70, 60));
+
+        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/Img/3.PNG"))); // NOI18N
+        jButton3.setText("jButton1");
+        jButton3.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 200, 70, 60));
+
+        jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/Img/4.PNG"))); // NOI18N
+        jButton4.setText("jButton1");
+        jButton4.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+        add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 200, 70, 60));
+
+        jButton5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/Img/5.PNG"))); // NOI18N
+        jButton5.setText("jButton1");
+        jButton5.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
+        add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 260, 70, 60));
+
+        jButton6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/Img/6.PNG"))); // NOI18N
+        jButton6.setText("jButton1");
+        jButton6.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jButton6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton6ActionPerformed(evt);
+            }
+        });
+        add(jButton6, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 260, 70, 60));
+
+        jButton7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/Img/7.PNG"))); // NOI18N
+        jButton7.setText("jButton1");
+        jButton7.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jButton7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton7ActionPerformed(evt);
+            }
+        });
+        add(jButton7, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 260, 70, 60));
+
+        jButton8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/Img/8.PNG"))); // NOI18N
+        jButton8.setText("jButton1");
+        jButton8.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jButton8.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton8ActionPerformed(evt);
+            }
+        });
+        add(jButton8, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 260, 70, 60));
+
+        jButton9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/Img/9.PNG"))); // NOI18N
+        jButton9.setText("jButton1");
+        jButton9.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jButton9.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton9ActionPerformed(evt);
+            }
+        });
+        add(jButton9, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 320, 70, 60));
+
+        jButton10.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/Img/10.PNG"))); // NOI18N
+        jButton10.setText("jButton1");
+        jButton10.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jButton10.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton10ActionPerformed(evt);
+            }
+        });
+        add(jButton10, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 320, 70, 60));
+
+        jButton12.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/Img/12.PNG"))); // NOI18N
+        jButton12.setText("jButton1");
+        jButton12.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jButton12.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton12ActionPerformed(evt);
+            }
+        });
+        add(jButton12, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 320, 70, 60));
+
+        jButton11.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/Img/11.PNG"))); // NOI18N
+        jButton11.setText("jButton1");
+        jButton11.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jButton11.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton11ActionPerformed(evt);
+            }
+        });
+        add(jButton11, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 320, 70, 60));
+
+        jButton13.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/Img/13.PNG"))); // NOI18N
+        jButton13.setText("jButton1");
+        jButton13.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jButton13.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton13ActionPerformed(evt);
+            }
+        });
+        add(jButton13, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 380, 70, 60));
+
+        jButton14.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/Img/14.PNG"))); // NOI18N
+        jButton14.setText("jButton1");
+        jButton14.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jButton14.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton14ActionPerformed(evt);
+            }
+        });
+        add(jButton14, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 380, 70, 60));
+
+        jButton16.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/Img/Buttons.PNG"))); // NOI18N
+        jButton16.setText("jButton1");
+        jButton16.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jButton16.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton16ActionPerformed(evt);
+            }
+        });
+        add(jButton16, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 380, 70, 60));
+
+        btnAlter.setText("ALTERNAR");
+        add(btnAlter, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 170, 110, 60));
+
+        jButton17.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/Img/15.PNG"))); // NOI18N
+        jButton17.setText("jButton1");
+        jButton17.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jButton17.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton17ActionPerformed(evt);
+            }
+        });
+        add(jButton17, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 380, 70, 60));
+
+        jLabel1.setBackground(new java.awt.Color(255, 204, 51));
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/Img/fr2.1.png"))); // NOI18N
+        jLabel1.setBorder(new javax.swing.border.MatteBorder(null));
+        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 170, 320, 300));
+
+        btnSolve.setText("RESOLVER");
+        add(btnSolve, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 240, 110, 60));
+
+        jLabel5.setText("Leonardo Gonzalez");
+        add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 60, -1, -1));
+
+        jLabel6.setText("David Clavijo");
+        add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 80, -1, -1));
+
+        jLabel7.setText("Movements");
+        add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 320, -1, -1));
+
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+        add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 340, 150, 130));
+
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/Img/monitorias.jpg"))); // NOI18N
+        jLabel4.setText("jLabel4");
+        add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 50, 190, 70));
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/Img/Wood-Wallpaper.jpg"))); // NOI18N
+        add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 590, 530));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -41,6 +264,74 @@ public class UserGUI extends java.awt.Frame {
     private void exitForm(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_exitForm
         System.exit(0);
     }//GEN-LAST:event_exitForm
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton5ActionPerformed
+
+    private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton6ActionPerformed
+
+    private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton7ActionPerformed
+
+    private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton8ActionPerformed
+
+    private void jButton9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton9ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton9ActionPerformed
+
+    private void jButton10ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton10ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton10ActionPerformed
+
+    private void jButton11ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton11ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton11ActionPerformed
+
+    private void jButton12ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton12ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton12ActionPerformed
+
+    private void jButton13ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton13ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton13ActionPerformed
+
+    private void jButton14ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton14ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton14ActionPerformed
+
+    private void jButton16ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton16ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton16ActionPerformed
+
+    private void jButton17ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton17ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton17ActionPerformed
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -55,5 +346,30 @@ public class UserGUI extends java.awt.Frame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAlter;
+    private javax.swing.JButton btnSolve;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton10;
+    private javax.swing.JButton jButton11;
+    private javax.swing.JButton jButton12;
+    private javax.swing.JButton jButton13;
+    private javax.swing.JButton jButton14;
+    private javax.swing.JButton jButton16;
+    private javax.swing.JButton jButton17;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
+    private javax.swing.JButton jButton7;
+    private javax.swing.JButton jButton8;
+    private javax.swing.JButton jButton9;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
